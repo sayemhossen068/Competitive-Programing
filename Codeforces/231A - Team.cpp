389ac3295c31f53f81580b7b31c1e31c9f161b74
sayemhossen068 <sayemhossen068@gmail.com>
2025-09-26 16:39:06 +0600

    Create 231A - Team.cpp

---
+++ Codeforces/231A - Team.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int n, a, b, c;
+    int count=0;
+    
+    cin >> n;
+    
+    for(int i=1; i<=n; i++){
+        cin >> a >> b >> c;
+        if(a+b+c >= 2){
+            count++;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
