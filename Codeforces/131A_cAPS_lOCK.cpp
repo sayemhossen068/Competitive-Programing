44d16094007298f455d2760f02b8e0fb718af285
sayemhossen068 <sayemhossen068@gmail.com>
2025-09-26 18:14:06 +0600

    Create 131A_cAPS_lOCK.cpp

---
+++ Codeforces/131A_cAPS_lOCK.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i;
+    string s;
+    cin >> s;
+    
+    for (i = 1; i < s.length(); i++)
+    {
+        if (islower(s[i]))
+        {
+            
+            break;
+        }
+    }
+    
+    if (i == s.length())
+    {
+        for (int j = 0; j < s.length(); j++)
+        {
+            if (islower(s[j]))
+                cout << (char)toupper(s[j]);
+            else
+                cout << (char)tolower(s[j]);
+        }
+    }
+    else
+    {
+        cout << s;
+    }
+
+    return 0;
+}
