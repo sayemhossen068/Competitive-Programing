9f9fecdb77d449c16b921b7c2827ffa6582d1268
sayemhossen068 <sayemhossen068@gmail.com>
2025-09-27 13:05:16 +0600

    Create 1115.cpp

---
+++ Beecrowd/Beginner./1115.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int x, y;
+    while(true){
+        cin >> x >> y;
+        if(x==0) break;
+        if(y==0) break;
+
+        if(x>0 && y>0){
+            cout << "primeiro" << endl;
+        } if(x>0 && y<0) {
+            cout << "quarto" << endl;
+        } if(x<0 && y<0) {
+            cout << "terceiro" << endl;
+        } if(x<0 && y>0) {
+            cout << "segundo" << endl;
+        }
+    }
+    return 0;
+}
