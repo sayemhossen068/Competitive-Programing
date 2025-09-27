94ab395ff4fdc394e26263b185471e18e1a26cb2
sayemhossen068 <sayemhossen068@gmail.com>
2025-09-27 13:05:43 +0600

    Create 1116.cpp

---
+++ Beecrowd/Beginner./1116.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main () {
+    int n;
+    double x, y;
+
+    cin >> n;
+
+    for(int i=0; i<n; i++) {
+        cin >> x >> y;
+        if(y == 0) {
+            cout << "divisao impossivel" << endl;
+        }
+        else {
+            cout << fixed << setprecision(1);
+            cout << x/y << endl;
+        }
+    }
+    return 0;
+}
