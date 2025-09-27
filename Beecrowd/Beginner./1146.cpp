e98a38606bec13b5f460e7d4b4e76a2c4be65b0a
sayemhossen068 <sayemhossen068@gmail.com>
2025-09-27 13:15:29 +0600

    Create 1146.cpp

---
+++ Beecrowd/Beginner./1146.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    while (cin >> n) {
+        if (n == 0) break;
+        for (int i = 1; i <= n; ++i) {
+            cout << i;
+            if (i < n) cout << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
