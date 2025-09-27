a599b113f6133f89d59c5299474f5513af70e47c
sayemhossen068 <sayemhossen068@gmail.com>
2025-09-27 13:03:12 +0600

    Create 1099.cpp

---
+++ Beecrowd/Beginner./1099.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    while (N--) {
+        int x, y;
+        cin >> x >> y;
+        if (x > y) swap(x, y);
+        int sum = 0;
+        for (int i = x + 1; i < y; ++i) {
+            if (i % 2 != 0) {
+                sum += i;
+            }
+        }
+        cout << sum << "\n";
+    }
+    return 0;
+}
