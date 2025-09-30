5ca614143e319d294ba88ee507dfe4df33298b17
sayemhossen068 <sayemhossen068@gmail.com>
2025-09-30 09:14:45 +0600

    Create 160A_Twins.cpp

---
+++ Codeforces/160A_Twins.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+
+    int n, coin, sum = 0, sum2 = 0, coincount=0;
+    cin >> n;
+    vector<int> coins;
+    for(int i=0; i<n; i++) {
+        cin >> coin;
+        coins.push_back(coin);
+        sum += coin;
+    }
+
+    sum = sum/2;
+    sort(coins.rbegin(), coins.rend());
+
+    for(auto v : coins) {
+        sum2 += v;
+        coincount++;
+        if(sum < sum2) {
+            cout << coincount << endl;
+            break;
+        }
+    }
+    
+}
