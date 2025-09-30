17c0ea0883dc5d3b03a678a215eebba856f8b18b
sayemhossen068 <sayemhossen068@gmail.com>
2025-09-30 11:58:14 +0600

    Create 61A_Ultra_Fast_Mathematician.cpp

---
+++ Codeforces/61A_Ultra_Fast_Mathematician.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    string a, b;
+    cin >> a >> b;
+
+    for(int i=0; i<a.size(); i++) {
+        int xOr = a[i] ^ b[i];
+        cout << xOr;
+    }
+}
+
+/* easier solution
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    string a, b;
+    cin >> a >> b;
+
+    for(int i=0; i<a.size(); i++) {
+        if(a[i] == b[i]) {
+            cout << "0";
+        } else {
+            cout << "1";
+        }
+    }
+}*/
