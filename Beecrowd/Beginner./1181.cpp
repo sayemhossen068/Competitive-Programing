49c863606ba6af60da49f5730a05a825999e7e13
sayemhossen068 <sayemhossen068@gmail.com>
2025-10-01 10:33:19 +0600

    Update 1181.cpp

--- Beecrowd/Beginner./1181.cpp
+++ Beecrowd/Beginner./1181.cpp
@@ -2,13 +2,12 @@
 using namespace std;
 
 int main() {
-    int L;          // row number
-    char T;         // operation (S or M)
+    int L;
+    char T;
     cin >> L >> T;
 
-    double M[12][12];   // 12x12 matrix
+    double M[12][12];
 
-    // Read matrix
     for(int i = 0; i < 12; i++) {
         for(int j = 0; j < 12; j++) {
             cin >> M[i][j];
@@ -17,7 +16,7 @@ int main() {
 
     double sum = 0.0;
     for(int j = 0; j < 12; j++) {
-        sum += M[L][j];   // take row L
+        sum += M[L][j];
     }
 
     if(T == 'S') {
