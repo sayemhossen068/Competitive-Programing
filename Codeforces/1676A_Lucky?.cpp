f477bc1e317863481556913d79b9ea319cd9dfdf
sayemhossen068 <sayemhossen068@gmail.com>
2025-10-04 22:38:50 +0600

    Create 1676A_Lucky?.cpp

---
+++ Codeforces/1676A_Lucky?.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    while(n--) {
+        string s;
+        cin >> s;
+
+        int sum1 = s[0]-'0' + s[1]-'0' + s[2]-'0';
+        int sum2 = s[3]-'0' + s[4]-'0' + s[5]-'0';
+
+        if(sum1 == sum2) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
