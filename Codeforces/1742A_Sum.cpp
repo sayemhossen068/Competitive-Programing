c69c42928cc9b88059862b514ab039e0daa555fb
sayemhossen068 <sayemhossen068@gmail.com>
2025-10-04 20:37:39 +0600

    Create 1742A_Sum.cpp

---
+++ Codeforces/1742A_Sum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, a, b, c;
+
+    cin >> n;
+
+    while(n--) {
+        cin >> a >> b >> c;
+
+        if(a+b==c || b+c == a || a+c ==b) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
