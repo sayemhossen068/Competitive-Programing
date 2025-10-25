07b056446b48f4735ce08654a28b90d586fb18f8
Sayem Hossen <sayemhossen068@gmail.com>
2025-10-25 22:32:22 +0600

    Create 1850A_To_My_Critics.cpp

---
+++ Codeforces/1850A_To_My_Critics.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int N;
+    cin >> N;
+ 
+    while (N--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if(a+b>=10 || b+c>=10 || c+a>=10) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+ 
+ 
+    return 0;
+}
