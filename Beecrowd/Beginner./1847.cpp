b18cdfe4fe3e5c66ccc4db8993e4793019db2d66
Sayem Hossen <sayemhossen068@gmail.com>
2025-10-25 23:05:53 +0600

    Create 1847.cpp

---
+++ Beecrowd/Beginner./1847.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+    
+    if (B > A && C <= B) cout << ":(" << endl;
+    else if (B < A && C >= B) cout << ":)" << endl;
+    else if (B > A && C > B && (C - B) < (B - A)) cout << ":(" << endl;
+    else if (B > A && C > B && (C - B) >= (B - A)) cout << ":)" << endl;
+    else if (B < A && C < B && (B - C) < (A - B)) cout << ":)" << endl;
+    else if (B < A && C < B && (B - C) >= (A - B)) cout << ":(" << endl;
+    else if (A == B && C > B) cout << ":)" << endl;
+    else cout << ":(" << endl;
+    
+    return 0;
+}
