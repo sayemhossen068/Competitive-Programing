066d1079daa0352cc7c3c8059612ef896d0554aa
Sayem Hossen <sayemhossen068@gmail.com>
2025-10-29 06:13:36 +0600

    Create 492B_Vanya_and_Lanterns.cpp

---
+++ Codeforces/492B_Vanya_and_Lanterns.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int a, b, m = 0;
+    cin >> a >> b;
+    vector<int> arr(a);
+    for(int i=0; i<a; i++){
+        cin >> arr[i];
+    }
+    
+    sort(arr.begin(), arr.end());
+    
+    for(int i=0; i<a-1; i++){
+        if(arr[i+1]-arr[i] > m){
+            m = arr[i+1] - arr[i];
+        }
+    }
+    
+    double x = arr[0];
+    double y = (double)m/2;
+    double ans = max(x,y);
+    double z = b - arr[a-1];
+    ans = max(z,ans);
+    
+    cout << fixed << setprecision(10);
+    cout << ans << endl;
+}
