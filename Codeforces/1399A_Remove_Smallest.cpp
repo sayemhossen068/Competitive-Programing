806a2442cf9e62d27125c7faccd3a1ba9e038c91
Sayem Hossen <sayemhossen068@gmail.com>
2025-11-03 18:18:58 +0600

    Create 1399A_Remove_Smallest.cpp

---
+++ Codeforces/1399A_Remove_Smallest.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		vector<int> a(n);
+		for (auto &it : a) cin >> it;
+		sort(a.begin(), a.end());
+		bool ok = true;
+		for (int i = 1; i < n; ++i) {
+			ok &= (a[i] - a[i - 1] <= 1);
+		}
+		if (ok) cout << "YES" << endl;
+		else cout << "NO" << endl;
+	}
+	
+	return 0;
+}
