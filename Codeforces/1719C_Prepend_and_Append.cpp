b798ea25f8993a7fea1e2a8941cdacffe5c951e9
Sayem Hossen <sayemhossen068@gmail.com>
2025-11-16 15:36:14 +0600

    Add solution for Codeforces problem 1719C

---
+++ Codeforces/1719C_Prepend_and_Append.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve() {
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	int l = 0, r = n - 1, ans = n;
+	while (s[l] != s[r] && ans > 0) {
+        l++; r--; ans -= 2;
+    }
+	cout << ans << '\n';
+}
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int tt;
+    cin >> tt;
+    for (int i = 1; i <= tt; i++) {
+        solve();
+    }
+}
