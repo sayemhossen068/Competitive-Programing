cd4471d1a8a4a568dcfaf136e86b563d00be3607
Sayem Hossen <sayemhossen068@gmail.com>
2025-12-06 17:59:45 +0600

    Create 1370A_Maximum_GCD.cpp

---
+++ Codeforces/1370A_Maximum_GCD.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int n;
+    cin >> n;
+
+    while(n--) {
+        int a;
+        cin >> a;
+        cout << a/2 << endl;
+    }
+    return 0;
+}
