859804f7afcb83ef42b9d89ca455af7b520a6e39
Sayem Hossen <sayemhossen068@gmail.com>
2025-12-12 11:41:44 +0600

    Create 2044A_Easy_Problem.cpp

---
+++ Codeforces/2044A_Easy_Problem.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve() {
+    int n;
+    cin >> n;
+    cout << (n-1) << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
