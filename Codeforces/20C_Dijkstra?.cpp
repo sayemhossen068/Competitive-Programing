4bee145d94536773439bcf141664d48346a16d0c
Sayem Hossen <sayemhossen068@gmail.com>
2025-12-31 23:20:44 +0600

    Create 20C_Dijkstra?.cpp

---
+++ Codeforces/20C_Dijkstra?.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+const ll INF = 1e18;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<pair<int,ll>>> adj(n + 1);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        ll w;
+        cin >> u >> v >> w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+
+    vector<ll> dist(n + 1, INF);
+    vector<int> parent(n + 1, -1);
+
+    priority_queue<pair<ll,int>, vector<pair<ll,int>>, greater<>> pq;
+    dist[1] = 0;
+    pq.push({0, 1});
+
+    while (!pq.empty()) {
+        auto [d, u] = pq.top();
+        pq.pop();
+
+        if (d > dist[u]) continue;
+
+        for (auto [v, w] : adj[u]) {
+            if (dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+                parent[v] = u;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+
+    if (dist[n] == INF) {
+        cout << -1 << "\n";
+        return 0;
+    }
+
+    vector<int> path;
+    for (int v = n; v != -1; v = parent[v])
+        path.push_back(v);
+
+    reverse(path.begin(), path.end());
+
+    for (int x : path)
+        cout << x << " ";
+    cout << "\n";
+
+    return 0;
+}
