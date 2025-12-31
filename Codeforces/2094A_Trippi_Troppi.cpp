562614f52606074f3fe3469d3552583f791fcc3f
Sayem Hossen <sayemhossen068@gmail.com>
2025-12-31 14:04:45 +0600

    Create 2094A_Trippi_Troppi.cpp

---
+++ Codeforces/2094A_Trippi_Troppi.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    string a, b, c;
+    cin >> a >> b >> c;
+    
+    cout << a[0] << b[0] << c[0] << endl;
+}
+
+int main (){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  
+  int n;
+  cin >> n;
+  
+  while(n--){
+      solve();
+  }
+  
+  return 0;
+}
