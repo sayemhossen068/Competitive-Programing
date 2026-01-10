adf8571376559b250bdc2e46879f5b68732dc507
Sayem Hossen <sayemhossen068@gmail.com>
2026-01-10 22:41:50 +0600

    Delete Beecrowd directory

--- Beecrowd/Beginner./1001.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main() {
-    int A,B,X;
-    scanf("%d %d", &A, &B);
-    X=A+B;
-    printf("X = %d\n",X);
-    return 0;
-}
--- Beecrowd/Beginner./1004.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    int x,y;
-    cin >> x >> y;
-    printf("PROD = %d\n", x*y);
-    return 0;
-}
