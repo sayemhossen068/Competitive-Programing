2f2d19100b95c8bcf6c0587db801a4646787a43c
Sayem Hossen <sayemhossen068@gmail.com>
2026-01-30 21:36:58 +0600

    Create 1075.java

---
+++ Beecrowd/1. Beginner/1075/1075.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        
+        for(int i=1; i<=10000; i++) {
+            if (i % n == 2) {
+                System.out.println(i);
+            }
+        }
+
+        input.close();
+    }
+}
