6164d412680a2597b64cf23d46cfd1ca1653af50
Sayem Hossen <sayemhossen068@gmail.com>
2026-01-30 21:35:59 +0600

    Create 1073.java

---
+++ Beecrowd/1. Beginner/1073/1073.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        
+        for(int i=2; i<=n; i++) {
+            if(i %2 == 0) {
+                System.out.println(i + "^2 = " + (int)Math.pow(i, 2));
+            }
+        }
+
+        input.close();
+    }
+}
