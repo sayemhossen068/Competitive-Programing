78a82124cda83e266daf95096be1549223950532
Sayem Hossen <sayemhossen068@gmail.com>
2026-04-18 20:15:56 +0600

    Create 1986A_X_Axis.cpp

---
+++ Codeforces/1986A_X_Axis.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int a, b, c;
+        cin >> a >> b >> c;
+        
+        int output = abs(min({a, b, c}) - max({a,b,c}));
+        
+        cout << output << endl;
+    }
+}
