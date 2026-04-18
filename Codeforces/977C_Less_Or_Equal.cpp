5a6bbcaf43146f0a577f0804c5bd795658ade003
Sayem Hossen <sayemhossen068@gmail.com>
2026-04-18 20:16:45 +0600

    Create 977C_Less_Or_Equal.cpp

---
+++ Codeforces/977C_Less_Or_Equal.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+
+    if(k == 0) {
+        if(a[0] == 1) {
+            cout << -1 << endl;
+        } else {
+            cout << a[0] - 1 << endl;
+        }
+    } else {
+        int x = a[k - 1];
+        if(k < n && a[k] == x) {
+            cout << -1 << endl;
+        } else {
+            cout << x << endl;
+        }
+    }
+
+    return 0;
+}
